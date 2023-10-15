671133650cffa1026d0a36ef00b254322fc49cd0
Aaron Chang <aaronchang153@gmail.com>
2023-10-14 22:39:24 -0700

    Change default tabstop

--- lua/myconfig/init.lua
+++ lua/myconfig/init.lua
@@ -9,8 +9,8 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = true
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.scrolloff = 2
