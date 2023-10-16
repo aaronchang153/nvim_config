8273fbbe864346f1498072688c5259f73a841600
Aaron Chang <aaronchang153@gmail.com>
2023-10-15 21:34:40 -0700

    Add descriptions for custom keybinds

--- after/plugin/nvim_tree.lua
+++ after/plugin/nvim_tree.lua
@@ -2,7 +2,7 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.keymap.set("n", "<leader>pe", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>pe", vim.cmd.NvimTreeToggle, {desc = "Toggle NvimTree"})
 
 require("nvim-tree").setup({
   renderer = {
--- lua/myconfig/remap.lua
+++ lua/myconfig/remap.lua
@@ -1,16 +1,16 @@
 vim.g.mapleader = " "
 
 -- Find and replace within file
-vim.keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>//gcI<Left><Left><Left><Left>")
-vim.keymap.set("n", "<leader>e", "yiw:%s/\\<<C-r>0\\>/<C-r>0/gcI<Left><Left><Left><Left>")
-vim.keymap.set("x", "<leader>r", "y:%s/\\<<C-r>0\\>//gcI<Left><Left><Left><Left>")
+vim.keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>//gcI<Left><Left><Left><Left>", {desc = "Find and replace"})
+vim.keymap.set("n", "<leader>e", "yiw:%s/\\<<C-r>0\\>/<C-r>0/gcI<Left><Left><Left><Left>", {desc = "Find and edit"})
+vim.keymap.set("x", "<leader>r", "y:%s/\\<<C-r>0\\>//gcI<Left><Left><Left><Left>", {desc = "Find and replace selection"})
 
 -- Move around selections
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move selection down"})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move selection up"})
 
 -- Buffer manip
-vim.keymap.set("n", "<leader>bb", ":ls<CR>:b<Space>")
-vim.keymap.set("n", "<leader>bd", ":ls<CR>:bd<Space>")
-vim.keymap.set("n", "<leader>bn", ":bn<CR>")
-vim.keymap.set("n", "<leader>bp", ":bp<CR>")
+vim.keymap.set("n", "<leader>bb", ":ls<CR>:b<Space>", {desc = "Switch buffer"})
+vim.keymap.set("n", "<leader>bd", ":ls<CR>:bd<Space>", {desc = "Delete buffer"})
+vim.keymap.set("n", "<leader>bn", ":bn<CR>", {desc = "Next buffer"})
+vim.keymap.set("n", "<leader>bp", ":bp<CR>", {desc = "Prev buffer"})
