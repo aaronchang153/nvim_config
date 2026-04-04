59a98d30671414fd1db963a4f38c555bc8619ae1
Aaron Chang <aaronchang153@gmail.com>
2026-04-04 14:23:42 -0700

    Hardcode indent line color

--- after/plugin/ibl.lua
+++ after/plugin/ibl.lua
@@ -1,6 +1,20 @@
 -- Indent Blankline
-require("ibl").setup({
-  scope = {
-    enabled = false,
-  }
-})
+
+local highlight = {
+    "CustomIndentHighlight",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "CustomIndentHighlight", { fg = "#313244" })
+end)
+
+require("ibl").setup {
+    indent = { highlight = highlight },
+    whitespace = {
+        highlight = highlight,
+    },
+    scope = { enabled = false },
+}
